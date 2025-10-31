29590bedb1bfe821949ceddd6b3fe17fabe07a4e
jament23 <129774653+jament23@users.noreply.github.com>
2025-10-31 14:23:29 -0400

    Update Eleventy config to copy all files

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,25 @@
 module.exports = function(eleventyConfig) {
 
-  // Tell Eleventy to copy the 'style.css' file 
-  // from our project to the final '_site' folder.
+  // --- 1. Copy Assets ---
+  // Copy the 'style.css' file to the final site
   eleventyConfig.addPassthroughCopy("style.css");
 
+  // --- 2. Copy Folders ---
+  // Copy these entire folders (and all their files) to the final site
+  eleventyConfig.addPassthroughCopy("attorneys");
+  eleventyConfig.addPassthroughCopy("practice-areas");
+  // eleventyConfig.addPassthroughCopy("blog"); // We'll uncomment this later
+
+  // --- 3. Process All HTML ---
+  // Tell Eleventy to find and process all .html files,
+  // not just index.html.
+  return {
+    templateFormats: [
+      "md",
+      "njk",
+      "html",
+      "liquid"
+    ],
+    htmlTemplateEngine: "liquid"
+  };
 };
